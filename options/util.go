b3b3dda6ddbeee87d1b0094edf79c85d848a546a
Rangel Reale <rangelspam@gmail.com>
2023-07-04 16:41:12 -0300

    options refactor

--- options/util.go
+++ options/util.go
@@ -1,16 +1,5 @@
 package options
 
-// // ExtractOptions extracts only options of a specific type.
-// func ExtractOptions[RET Option, IN ~[]Option](options IN) []RET {
-// 	var ret []RET
-// 	for _, opt := range options {
-// 		if o, ok := opt.(RET); ok {
-// 			ret = append(ret, o)
-// 		}
-// 	}
-// 	return ret
-// }
-
 // ExtractOptions extracts only options of a specific type.
 func ExtractOptions[RET any, INT any, IN ~[]INT](options IN) []RET {
 	var ret []RET
@@ -21,11 +10,6 @@ func ExtractOptions[RET any, INT any, IN ~[]INT](options IN) []RET {
 	}
 	return ret
 }
-
-// // ConcatOptionsBefore returns an array with "options" before "source".
-// func ConcatOptionsBefore[T Option](source []T, options ...T) []T {
-// 	return append(append([]T{}, options...), source...)
-// }
 
 // ConcatOptionsBefore returns an array with "options" before "source".
 func ConcatOptionsBefore[T any](source []T, options ...T) []T {
